0edfc4b2cdeeca04192447dfa616975f10a1ba46
Himanshu Gunwant <himanshu.gunwant@acefone.com>
2024-10-22 10:53:56 +0530

    add logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,7 +1,27 @@
 package logger
 
 import (
-	"github.com/sirupsen/logrus"
+	"os"
+
+	log "github.com/sirupsen/logrus"
 )
 
-var Logger = logrus.New()
+var Logger = log.New()
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	Logger.SetFormatter(&log.TextFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	Logger.SetOutput(os.Stdout)
+
+	// You could set this to any `io.Writer` such as a file
+	// file, err := os.OpenFile("logs/logrus.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	// if err == nil {
+	// 	Logger.Out = file
+	// } else {
+	// 	Logger.Info("Failed to log to file, using default stderr")
+	// }
+
+}
